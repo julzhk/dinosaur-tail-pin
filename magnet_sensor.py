56743b791a2a4c2c18bfc8e9c7287e25440a9c25
julian harley <julian.harley.fr@harpercollins.co.uk>
2019-12-17 20:51:54 +0000

    formatting

--- magnet_sensor.py
+++ magnet_sensor.py
@@ -19,13 +19,10 @@ class SoundPlayer(object):
         'TRexRoar.mp3'
     ]
 
-    def play(self,
- filename=None):
+    def play(self, filename=None):
         if filename is None:
             filename = choice(self.soundfiles)
-        self.player = subprocess.Popen(["omxplayer",
-                                        filename,
-                                        ],
+        self.player = subprocess.Popen(["omxplayer",filename,],
                                   stdin=subprocess.PIPE,
                                   stdout=subprocess.PIPE,
                                   stderr=subprocess.PIPE)
@@ -37,10 +34,8 @@ class Rolling_Avg(deque):
  pops oldest item
     This class adds the 'avg' rolling average calculation
     r = Rolling_Avg(max_len=3)
-    r.append(1)
-    r.append(2)
-    r.append(3)
-    r.append(4)
+    r.append(1); r.append(2)
+    r.append(3); r.append(4)
     r.avg()
     >> 4.5
     """
@@ -50,9 +45,7 @@ class Rolling_Avg(deque):
 def intensity(*args):
     """ given some numbers,
  average of the squares of them:
-    intensity(1,
-2,
-2)
+    intensity(1, 2, 2)
     >>> 3.0
     """
     r = sum([a * a for a in args]) / len(args)
@@ -69,13 +62,12 @@ while True:
     curr = intensity(*raw.values())
     rolling_av.append(curr)
     rolling_av_avg = rolling_av.avg()
-    print('{:.0f}'.format(curr - rolling_av_avg))
+    # print('{:.0f}'.format(curr - rolling_av_avg))
     if not is_quiet and curr < rolling_av_avg * 1.2:
         print('go quiet')
         player.stop()
         is_quiet=True
-
     if is_quiet and curr > rolling_av_avg * 1.5:
         print('roar')
-        is_quiet = False
         player.play()
+        is_quiet = False
